3210443bcfa583a23e2a4113d623cde294d4c34e
Hosam <Hosamactionuser@outlook.com>
2026-06-09 16:12:12 +0300

    hello

--- src/main/java/net/hosam/microbes/Microbes.java
+++ src/main/java/net/hosam/microbes/Microbes.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//hello
 public class Microbes implements ModInitializer {
 	public static final String MOD_ID = "microbes";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
