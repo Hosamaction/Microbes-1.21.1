c34b7cbc48921f1d67db5405114cb90b80175921
Hosam <Hosamactionuser@outlook.com>
2026-06-09 15:46:24 +0300

    Initial commit

---
+++ src/main/java/net/hosam/microbes/Microbes.java
@@ -0,0 +1,16 @@
+package net.hosam.microbes;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Microbes implements ModInitializer {
+	public static final String MOD_ID = "microbes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/hosam/microbes/MicrobesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hosam.microbes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MicrobesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
