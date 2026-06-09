6281b3b6bc46993b5eeb704dfb39302f4e8dc67e
Hosam <Hosamactionuser@outlook.com>
2026-06-09 18:44:17 +0300

    Swab update

--- src/main/java/net/hosam/microbes/Microbes.java
+++ src/main/java/net/hosam/microbes/Microbes.java
@@ -2,6 +2,7 @@ package net.hosam.microbes;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.hosam.microbes.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Microbes implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/hosam/microbes/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.hosam.microbes.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.hosam.microbes.Microbes;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+public class ModItems {
+    public static final Item SWAB = registerItem("swab", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Microbes.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Microbes.LOGGER.info("Registering Mod Items for " + Microbes.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SWAB);
+        });
+    }
+}
